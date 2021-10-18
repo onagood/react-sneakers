a9627d27349b41f938ec64510e0e215e85a72e52
onagood <onagood@yandex.ru>
2021-10-18 10:07:06 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div className="wrapper">
+    <header className="flex justify-between p-11">
+      <div className="flex items-center">
+        <img className="mr-4" width={40} height={40} src="/img/logo.png" alt="logo"/>
+        <div>
+          <h3><b>React Sneakers</b></h3>
+          <p>Магазин лучших кроссовок</p>
+        </div>
+      </div>
+      <ul className="flex items-center">
+        <li className="mr-8">
+          <img className="inline" width={18} height={18} src="/img/cart.svg" alt="cart"/>
+          <span>1205 руб.</span>
+        </li>
+        <li>
+          <img className="inline" width={18} height={18} src="/img/user.svg" alt="user"/>
+        </li>
+      </ul>
+    </header>
+  </div>
   );
 }
 
-export default App;
+export default App; 

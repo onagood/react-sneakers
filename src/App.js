7f0b29716d8cf509e3bc9fb75acae36f573aca41
onagood <onagood@yandex.ru>
2021-10-24 13:04:00 +0300

    fix img, add right-cart-bar, add search input

--- src/App.js
+++ src/App.js
@@ -1,6 +1,55 @@
 function App() {
   return (
   <div className="wrapper pb-10">
+    <div className="overlay">
+      <div className="drawer flex flex-col">
+        <div className="flex justify-between items-center mb-10">
+          <h2 className="font-bold text-2xl">Корзина</h2>
+          <button className="cart_remove">
+                <img width={11} height={11} className="m-auto" src="/img/remove.svg" alt="remove"/>
+          </button>
+        </div>
+        <div className="flex-grow overflow-auto">
+          <div className="cartItem flex items-center justify-between mb-5">
+            <img width={70} height={70} className="" src="img/sneakers/image1.png" alt="sneakers" />
+            <div className="text-sm w-1/2">
+              <p>Мужские Кроссовки Nike Air Max 270</p>
+              <p className="font-bold">12999 руб.</p>
+            </div>
+            <button className="cart_remove">
+                <img width={11} height={11} className="m-auto" src="/img/remove.svg" alt="remove"/>
+            </button>
+          </div>
+
+          <div className="cartItem flex items-center justify-between mb-5">
+            <img width={70} height={70} className="" src="img/sneakers/image1.png" alt="sneakers" />
+            <div className="text-sm w-1/2">
+              <p>Мужские Кроссовки Nike Air Max 270</p>
+              <p className="font-bold">12999 руб.</p>
+            </div>
+            <button className="cart_remove">
+                <img width={11} height={11} className="m-auto" src="/img/remove.svg" alt="remove"/>
+            </button>
+          </div>
+          
+        </div>
+        <div className="flex flex-col">
+          <ul>
+            <li className="flex justify-between mb-5">
+              <span>Итого:</span>
+              <b>21498 руб.</b>
+            </li>
+            <li className="flex justify-between mb-5">
+              <span>Налог 5%:</span>
+              <b>1074 руб.</b>
+            </li>
+          </ul>
+          <button className="greenBtn">Оформить заказ
+            <img className="inline" src="img/arrow.svg" alt="arrow" />
+          </button>
+        </div>
+      </div>
+    </div>
     {/*HEADER*/}
     <header className="xs_header flex sm:justify-between sm:p-11 border-b border-gray-300">
       <div className="xs_logo flex items-center">
@@ -26,16 +75,22 @@ function App() {
     {/*END HEADER*/}
     {/*CONTAINER*/}
     <div className="container mx-auto mt-9 sm:container sm:mx-auto md:container md:mx-auto lg:px-14">
-      <h1 className="md:ml-5 font-bold text-3xl leading-7 uppercase">Все Кроссовки</h1>
+      <div className="flex justify-between items-center searchAndTittle">
+        <h1 className="md:ml-5 font-bold text-3xl leading-7 uppercase">Все Кроссовки</h1>
+        <div className="search-block flex">
+          <img src="/img/search.svg" alt="search"/>
+          <input type="text" className="text-base" placeholder="Поиск..."/>
+        </div>
+      </div>
       
-      <div className="mt-9 xs_cart_grid grid gap-10 sm:gap-5 sm:justify-items-center sm:grid-cols-2 md:gap-10 md:grid-cols-3 lg:grid-cols-4">
-        {/*CART*/}
-        <div className="px-7 py-6 flex flex-col cart">
+      <div className="mt-9 xs_card_grid grid gap-10 sm:gap-5 sm:justify-items-center sm:grid-cols-2 md:gap-10 md:grid-cols-3 lg:grid-cols-4">
+        {/*card*/}
+        <div className="px-7 py-6 flex flex-col card">
           <div className="relative">
-            <button className="absolute cart_buttons">
+            <button className="absolute card_buttons">
               <img width={14} height={13} className="m-auto" src="/img/like.svg" alt="like"/>
             </button>
-            <img width={133} height={112} className="m-auto mb-3.5" src="/img/sneakers/image 1.png" alt="sneakers"/>  
+            <img width={133} height={112} className="m-auto mb-3.5" src="/img/sneakers/image1.png" alt="sneakers"/>  
           </div>
           <p className="text-sm mb-3.5 font-normal">Мужские Кроссовки Nike Blazer Mid Suede</p>
           <div className="flex justify-between">
@@ -43,18 +98,18 @@ function App() {
               <p className="text-xs font-medium text-gray-400">Цена</p>
               <p className="text-sm text-gray-900 font-bold">12 999 руб.</p>
             </div>
-            <button className="cart_buttons">
+            <button className="card_buttons">
               <img width={11} height={11} className="m-auto" src="/img/plus.svg" alt=""/>
             </button>
           </div>
         </div>
 
-        <div className="px-7 py-6 flex flex-col cart">
+        <div className="px-7 py-6 flex flex-col card">
           <div className="relative">
-            <button className="absolute cart_buttons">
+            <button className="absolute card_buttons">
               <img width={14} height={13} className="m-auto" src="/img/like.svg" alt="like"/>
             </button>
-            <img width={133} height={112} className="m-auto mb-3.5" src="/img/sneakers/image 2.png" alt="sneakers"/>  
+            <img width={133} height={112} className="m-auto mb-3.5" src="/img/sneakers/image2.png" alt="sneakers"/>  
           </div>
           <p className="text-sm mb-3.5 font-normal">Мужские Кроссовки Nike Air Max 270</p>
           <div className="flex justify-between">
@@ -62,18 +117,18 @@ function App() {
               <p className="text-xs font-medium text-gray-400">Цена</p>
               <p className="text-sm text-gray-900 font-bold">12 999 руб.</p>
             </div>
-            <button className="cart_buttons">
+            <button className="card_buttons">
               <img width={11} height={11} className="m-auto" src="/img/plus.svg" alt=""/>
             </button>
           </div>
         </div>
 
-        <div className="px-7 py-6 flex flex-col cart">
+        <div className="px-7 py-6 flex flex-col card">
           <div className="relative">
-            <button className="absolute cart_buttons">
+            <button className="absolute card_buttons">
               <img width={14} height={13} className="m-auto" src="/img/like.svg" alt="like"/>
             </button>
-            <img width={133} height={112} className="m-auto mb-3.5" src="/img/sneakers/image 3.png" alt="sneakers"/>  
+            <img width={133} height={112} className="m-auto mb-3.5" src="/img/sneakers/image3.png" alt="sneakers"/>  
           </div>
           <p className="text-sm mb-3.5 font-normal">Мужские Кроссовки Nike Blazer Mid Suede</p>
           <div className="flex justify-between">
@@ -81,18 +136,18 @@ function App() {
               <p className="text-xs font-medium text-gray-400">Цена</p>
               <p className="text-sm text-gray-900 font-bold">12 999 руб.</p>
             </div>
-            <button className="cart_buttons">
+            <button className="card_buttons">
               <img width={11} height={11} className="m-auto" src="/img/plus.svg" alt=""/>
             </button>
           </div>
         </div>
 
-        <div className="px-7 py-6 flex flex-col cart">
+        <div className="px-7 py-6 flex flex-col card">
           <div className="relative">
-            <button className="absolute cart_buttons">
+            <button className="absolute card_buttons">
               <img width={14} height={13} className="m-auto" src="/img/like.svg" alt="like"/>
             </button>
-            <img width={133} height={112} className="m-auto mb-3.5" src="/img/sneakers/image 4.png" alt="sneakers"/>  
+            <img width={133} height={112} className="m-auto mb-3.5" src="/img/sneakers/image4.png" alt="sneakers"/>  
           </div>
           <p className="text-sm mb-3.5 font-normal">Кроссовки Puma X Aka Boku Future Rider</p>
           <div className="flex justify-between">
@@ -100,18 +155,18 @@ function App() {
               <p className="text-xs font-medium text-gray-400">Цена</p>
               <p className="text-sm text-gray-900 font-bold">12 999 руб.</p>
             </div>
-            <button className="cart_buttons">
+            <button className="card_buttons">
               <img width={11} height={11} className="m-auto" src="/img/plus.svg" alt=""/>
             </button>
           </div>
         </div>
 
-        <div className="px-7 py-6 flex flex-col cart">
+        <div className="px-7 py-6 flex flex-col card">
           <div className="relative">
-            <button className="absolute cart_buttons">
+            <button className="absolute card_buttons">
               <img width={14} height={13} className="m-auto" src="/img/like.svg" alt="like"/>
             </button>
-            <img width={133} height={112} className="m-auto mb-3.5" src="/img/sneakers/image 5.png" alt="sneakers"/>  
+            <img width={133} height={112} className="m-auto mb-3.5" src="/img/sneakers/image5.png" alt="sneakers"/>  
           </div>
           <p className="text-sm mb-3.5 font-normal">Мужские Кроссовки Under Armour Curry 8</p>
           <div className="flex justify-between">
@@ -119,18 +174,18 @@ function App() {
               <p className="text-xs font-medium text-gray-400">Цена</p>
               <p className="text-sm text-gray-900 font-bold">15 199 руб.</p>
             </div>
-            <button className="cart_buttons">
+            <button className="card_buttons">
               <img width={11} height={11} className="m-auto" src="/img/plus.svg" alt=""/>
             </button>
           </div>
         </div>
 
-        <div className="px-7 py-6 flex flex-col cart">
+        <div className="px-7 py-6 flex flex-col card">
           <div className="relative">
-            <button className="absolute cart_buttons">
+            <button className="absolute card_buttons">
               <img width={14} height={13} className="m-auto" src="/img/like.svg" alt="like"/>
             </button>
-            <img width={133} height={112} className="m-auto mb-3.5" src="/img/sneakers/image 6.png" alt="sneakers"/>  
+            <img width={133} height={112} className="m-auto mb-3.5" src="/img/sneakers/image6.png" alt="sneakers"/>  
           </div>
           <p className="text-sm mb-3.5 font-normal">Мужские Кроссовки Nike Kyrie 7</p>
           <div className="flex justify-between">
@@ -138,18 +193,18 @@ function App() {
               <p className="text-xs font-medium text-gray-400">Цена</p>
               <p className="text-sm text-gray-900 font-bold">11 299 руб.</p>
             </div>
-            <button className="cart_buttons">
+            <button className="card_buttons">
               <img width={11} height={11} className="m-auto" src="/img/plus.svg" alt=""/>
             </button>
           </div>
         </div>
 
-        <div className="px-7 py-6 flex flex-col cart">
+        <div className="px-7 py-6 flex flex-col card">
           <div className="relative">
-            <button className="absolute cart_buttons">
+            <button className="absolute card_buttons">
               <img width={14} height={13} className="m-auto" src="/img/like.svg" alt="like"/>
             </button>
-            <img width={133} height={112} className="m-auto mb-3.5" src="/img/sneakers/image 7.png" alt="sneakers"/>  
+            <img width={133} height={112} className="m-auto mb-3.5" src="/img/sneakers/image7.png" alt="sneakers"/>  
           </div>
           <p className="text-sm mb-3.5 font-normal">Мужские Кроссовки Jordan Air Jordan 11</p>
           <div className="flex justify-between">
@@ -157,18 +212,18 @@ function App() {
               <p className="text-xs font-medium text-gray-400">Цена</p>
               <p className="text-sm text-gray-900 font-bold">10 799 руб.</p>
             </div>
-            <button className="cart_buttons">
+            <button className="card_buttons">
               <img width={11} height={11} className="m-auto" src="/img/plus.svg" alt=""/>
             </button>
           </div>
         </div>
 
-        <div className="px-7 py-6 flex flex-col cart">
+        <div className="px-7 py-6 flex flex-col card">
           <div className="relative">
-            <button className="absolute cart_buttons">
+            <button className="absolute card_buttons">
               <img width={14} height={13} className="m-auto" src="/img/like.svg" alt="like"/>
             </button>
-            <img width={133} height={112} className="m-auto mb-3.5" src="/img/sneakers/image 8.png" alt="sneakers"/>  
+            <img width={133} height={112} className="m-auto mb-3.5" src="/img/sneakers/image8.png" alt="sneakers"/>  
           </div>
           <p className="text-sm mb-3.5 font-normal">Мужские Кроссовки Nike LeBron XVIII</p>
           <div className="flex justify-between">
@@ -176,18 +231,18 @@ function App() {
               <p className="text-xs font-medium text-gray-400">Цена</p>
               <p className="text-sm text-gray-900 font-bold">16 499 руб.</p>
             </div>
-            <button className="cart_buttons">
+            <button className="card_buttons">
               <img width={11} height={11} className="m-auto" src="/img/plus.svg" alt=""/>
             </button>
           </div>
         </div>
 
-        <div className="px-7 py-6 flex flex-col cart">
+        <div className="px-7 py-6 flex flex-col card">
           <div className="relative">
-            <button className="absolute cart_buttons">
+            <button className="absolute card_buttons">
               <img width={14} height={13} className="m-auto" src="/img/like.svg" alt="like"/>
             </button>
-            <img width={133} height={112} className="m-auto mb-3.5" src="/img/sneakers/image 9.png" alt="sneakers"/>  
+            <img width={133} height={112} className="m-auto mb-3.5" src="/img/sneakers/image9.png" alt="sneakers"/>  
           </div>
           <p className="text-sm mb-3.5 font-normal">Мужские Кроссовки Nike Lebron XVIII Low</p>
           <div className="flex justify-between">
@@ -195,18 +250,18 @@ function App() {
               <p className="text-xs font-medium text-gray-400">Цена</p>
               <p className="text-sm text-gray-900 font-bold">13 999 руб.</p>
             </div>
-            <button className="cart_buttons">
+            <button className="card_buttons">
               <img width={11} height={11} className="m-auto" src="/img/plus.svg" alt=""/>
             </button>
           </div>
         </div>
 
-        <div className="px-7 py-6 flex flex-col cart">
+        <div className="px-7 py-6 flex flex-col card">
           <div className="relative">
-            <button className="absolute cart_buttons">
+            <button className="absolute card_buttons">
               <img width={14} height={13} className="m-auto" src="/img/like.svg" alt="like"/>
             </button>
-            <img width={133} height={112} className="m-auto mb-3.5" src="/img/sneakers/image 10.png" alt="sneakers"/>  
+            <img width={133} height={112} className="m-auto mb-3.5" src="/img/sneakers/image10.png" alt="sneakers"/>  
           </div>
           <p className="text-sm mb-3.5 font-normal">Мужские Кроссовки Nike Blazer Mid Suede</p>
           <div className="flex justify-between">
@@ -214,18 +269,18 @@ function App() {
               <p className="text-xs font-medium text-gray-400">Цена</p>
               <p className="text-sm text-gray-900 font-bold">8 499 руб.</p>
             </div>
-            <button className="cart_buttons">
+            <button className="card_buttons">
               <img width={11} height={11} className="m-auto" src="/img/plus.svg" alt=""/>
             </button>
           </div>
         </div>
 
-        <div className="px-7 py-6 flex flex-col cart">
+        <div className="px-7 py-6 flex flex-col card">
           <div className="relative">
-            <button className="absolute cart_buttons">
+            <button className="absolute card_buttons">
               <img width={14} height={13} className="m-auto" src="/img/like.svg" alt="like"/>
             </button>
-            <img width={133} height={112} className="m-auto mb-3.5" src="/img/sneakers/image 11.png" alt="sneakers"/>  
+            <img width={133} height={112} className="m-auto mb-3.5" src="/img/sneakers/image11.png" alt="sneakers"/>  
           </div>
           <p className="text-sm mb-3.5 font-normal">Кроссовки Puma X Aka Boku Future Rider</p>
           <div className="flex justify-between">
@@ -233,18 +288,18 @@ function App() {
               <p className="text-xs font-medium text-gray-400">Цена</p>
               <p className="text-sm text-gray-900 font-bold">8 999 руб.</p>
             </div>
-            <button className="cart_buttons">
+            <button className="card_buttons">
               <img width={11} height={11} className="m-auto" src="/img/plus.svg" alt=""/>
             </button>
           </div>
         </div>
 
-        <div className="px-7 py-6 flex flex-col cart">
+        <div className="px-7 py-6 flex flex-col card">
           <div className="relative">
-            <button className="absolute cart_buttons">
+            <button className="absolute card_buttons">
               <img width={14} height={13} className="m-auto" src="/img/like.svg" alt="like"/>
             </button>
-            <img width={133} height={112} className="m-auto mb-3.5" src="/img/sneakers/image 12.png" alt="sneakers"/>  
+            <img width={133} height={112} className="m-auto mb-3.5" src="/img/sneakers/image12.png" alt="sneakers"/>  
           </div>
           <p className="text-sm mb-3.5 font-normal">Мужские Кроссовки Nike Kyrie Flytrap IV</p>
           <div className="flex justify-between">
@@ -252,12 +307,12 @@ function App() {
               <p className="text-xs font-medium text-gray-400">Цена</p>
               <p className="text-sm text-gray-900 font-bold">11 299 руб.</p>
             </div>
-            <button className="cart_buttons">
+            <button className="card_buttons">
               <img width={11} height={11} className="m-auto" src="/img/plus.svg" alt=""/>
             </button>
           </div>
         </div>
-        {/*END CART*/}
+        {/*END card*/}
       </div>
     </div>
     {/*END CONTAINER*/}
